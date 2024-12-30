1c6cd1dfb1e6dc0a338236a53cadd48b9d5d1882
KruthikaPPatil <patilkruthikap@gmail.com>
2024-12-30 13:17:56 +0530

    login and register page

---
+++ src/main/java/com/org/jsp_gram/controller/AppController.java
@@ -0,0 +1,23 @@
+package com.org.jsp_gram.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	
+	  @GetMapping({"/","/login"})
+	  public String loadLogin() {
+		  return "login.html";
+	  }
+	  
+	  
+	  @GetMapping("/register")
+	  public String loadRegister() {
+		  return "register.html";
+	  }
+	  
+	  
+	  
+}
